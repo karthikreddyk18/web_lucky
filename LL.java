4a707ff08cef2fe05c98ec1cbee5b0f69052eb0b
karthikreddyk18 <b23it011@kitsw.ac.in>
2026-02-05 20:34:37 +0530

    added

---
+++ LL.java
@@ -0,0 +1,68 @@
+//876
+import java.util.Scanner;
+
+public class LL{
+
+    // Linked List Node
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    // Slow–Fast Pointer to find middle
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Print list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+
+        System.out.println("Enter node values:");
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode current = head;
+
+        for (int i = 1; i < n; i++) {
+            current.next = new ListNode(sc.nextInt());
+            current = current.next;
+        }
+
+        System.out.print("Full Linked List: ");
+        printList(head);
+
+        ListNode middle = middleNode(head);
+
+        System.out.print("Middle node onwards: ");
+        printList(middle);
+
+        sc.close();
+    }
+}
