00cd949d6097d0649edab3ddd0274df98bb7204c
karthikreddyk18 <b23it011@kitsw.ac.in>
2026-04-04 11:44:52 +0530

    Initial commit

--- node js/index3.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.listen(8080, () => console.log("Web Server has been started"));
-app.get("/", (req, res) => {
-console.log(req.url);
-console.log(req.method)
-console.log(req.body)
-res.send("response from server");
-});
